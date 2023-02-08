244fc5835a3ef6048ce926760bcce1381fee8c47
Alisson Ross <alisson@alisson.me>
2023-02-08 14:50:04 -0800

    Added NavBar component and updated package.json with MUI

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import About from './Components/About.js';
 import FAQ from './Components/FAQ.js'
+import NavBar from './Components/NavBar.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar/>
         <p>
           HackMerced VIII Page V.0.0
         </p>
---
+++ src/Components/NavBar.js
@@ -0,0 +1,9 @@
+function NavBar() {
+    return (
+      <div className="NavBar">
+            HackMerced VIII NavBar Component
+      </div>
+    );
+  }
+  
+  export default NavBar;
