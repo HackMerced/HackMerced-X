966e4ed002f5fb78cdbc879c1b3e1e19d2e3129d
Alisson Ross <alisson@alisson.me>
2023-02-08 17:48:19 -0800

    Styling changes- background, added Cards, text,svg

--- src/Components/Tracks.js
+++ src/Components/Tracks.js
@@ -1,7 +1,30 @@
+import { Card } from "@mui/material";
+import Support from '../Assets/support_1.svg'
 function Tracks() {
     return (
       <div className="Tracks">
-            HackMerced VIII Tracks Component
+           <h1 style={{marginTop: '0px'}}>Tracks</h1>
+           <div className="Tracks-Flex">
+            <Card className='Track-Card'>
+              <img src={Support}/>
+                <div className='Track-Text'>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae est semper, semper velit at, elementum mauris. Maecenas vel molestie magna. Praesent malesuada lacinia nibh ut fringilla. Morbi sed urna ac lectus eleifend pellentesque. Maecenas vel fringilla tellus. Etiam non massa sit amet velit semper placerat efficitur vel dolor. In turpis lectus, ultricies quis pellentesque et, malesuada ac nunc. Integer maximus elit et bibendum tristique. Fusce quam nunc, aliquet sed lectus ut, eleifend volutpat est. In venenatis a quam et tincidunt. Maecenas velit dui, maximus vel urna in, faucibus ultrices velit. Proin sit amet quam mi. Sed ut massa id velit dignissim ultricies ut id ante.
+                </div>
+              </Card>
+              <Card className='Track-Card'>
+              <img src={Support}/>
+                <div className='Track-Text'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae est semper, semper velit at, elementum mauris. Maecenas vel molestie magna. Praesent malesuada lacinia nibh ut fringilla. Morbi sed urna ac lectus eleifend pellentesque. Maecenas vel fringilla tellus. Etiam non massa sit amet velit semper placerat efficitur vel dolor. In turpis lectus, ultricies quis pellentesque et, malesuada ac nunc. Integer maximus elit et bibendum tristique. Fusce quam nunc, aliquet sed lectus ut, eleifend volutpat est. In venenatis a quam et tincidunt. Maecenas velit dui, maximus vel urna in, faucibus ultrices velit. Proin sit amet quam mi. Sed ut massa id velit dignissim ultricies ut id ante.
+                </div>
+              </Card>
+              <Card className='Track-Card'>
+              <img src={Support} alt="Support logo"/>
+                <div className='Track-Text'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae est semper, semper velit at, elementum mauris. Maecenas vel molestie magna. Praesent malesuada lacinia nibh ut fringilla. Morbi sed urna ac lectus eleifend pellentesque. Maecenas vel fringilla tellus. Etiam non massa sit amet velit semper placerat efficitur vel dolor. In turpis lectus, ultricies quis pellentesque et, malesuada ac nunc. Integer maximus elit et bibendum tristique. Fusce quam nunc, aliquet sed lectus ut, eleifend volutpat est. In venenatis a quam et tincidunt. Maecenas velit dui, maximus vel urna in, faucibus ultrices velit. Proin sit amet quam mi. Sed ut massa id velit dignissim ultricies ut id ante.
+                </div>
+              </Card>
+           </div>
+            
       </div>
     );
   }
