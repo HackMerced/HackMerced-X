8166a0fa9723325f486789e29afb9f5704823a11
Alisson Ross <alisson@alisson.me>
2023-02-28 23:43:25 -0800

    Added sponsors component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tracks from './Components/Tracks.js';
 import FAQ from './Components/FAQ.js'
 import NavBar from './Components/NavBar.js';
 import MainPage from './Components/MainPage.js';
+import Sponsors from './Components/Sponsors.js';
 import Footer from './Components/Footer.js';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <MainPage/>
       <Tracks/>
       <FAQ/>
+      <Sponsors/>
       <footer><Footer/></footer>
     </div>
   );
---
+++ src/Components/Sponsors.js
@@ -0,0 +1,15 @@
+import SponsorLogo from '../Assets/sponsor logos.svg';
+
+function Sponsors() {
+    return (
+      <div className="Tracks">
+        <h2 className ="Sponsors-title">sponsors</h2>
+            <div className="Sponsors-Description">
+                HackMerced is 100% run by student volunteers. Our events are only made possible through the support of our generous sponsors, whom enable the growth of technology in the San Joaquin Valley.   
+            </div>
+        <img className='Sponsors-Logo' src={SponsorLogo} alt="sponsors logos"/>
+      </div>
+    );
+  }
+  
+  export default Sponsors;
