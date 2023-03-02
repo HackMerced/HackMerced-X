9518361fef94fb063511c24f6ffa4e2ef62035a6
AlissonRoss <alirosssalazar@gmail.com>
2023-03-02 13:30:38 -0800

    added sponsors to navbar and updated tracks

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,6 +4,7 @@ function NavBar() {
         <ul className="nav-items">
           <li><a href="#tracks"><button className="button">tracks</button></a></li>
           <li><a href="#faq"><button className="button">faq</button></a></li>
+          <li><a href="#sponsors"><button className="button">sponsors</button></a></li>
           <li><a href="https://forms.gle/mgBKuK3G9L6FsxsJ8"><button className="button">mentor</button></a></li>
           <li><a href="https://forms.gle/pmMz4zeeEsMD3Pc1A"><button className="registerButton">register</button></a></li>
         </ul>
--- src/Components/Tracks.js
+++ src/Components/Tracks.js
@@ -1,29 +1,32 @@
 import { Card } from "@mui/material";
-import Support from '../Assets/support_1.svg'
+import Health from '../Assets/health.svg';
+import AG from '../Assets/ag.svg';
+import Business from '../Assets/bus.svg';
 function Tracks() {
     return (
       <div className="Tracks" id="tracks">
            <h2 className ="Tracks-title">tracks</h2>
            <div className="Tracks-Flex">
             <Card style={{borderRadius: '30px'}} className='Track-Card'>
-              <img className='Track-Logo' src={Support} alt="Support logo"/>
-              <h1 className="Track-Name">Coming soon</h1>
+              <img className='Track-Logo' src={Health} alt="Health logo"/>
+              <h1 className="Track-Name">health & wellness</h1>
                 <div className='Track-Text'>
-                Stay tuned to learn more about our prize tracks for this year.
+                Develop a technology that supports physical or mental healthcare, healthcare administration, or the promotion of healthy livelihoods. Address widespread concerns in health technology or support everyday wellbeing.
                 </div>
               </Card>
               <Card style={{borderRadius: '30px'}} className='Track-Card'>
-              <img className='Track-Logo' src={Support} alt="Support logo"/>
-              <h1 className="Track-Name">Coming soon</h1>
+              <img className='Track-Logo' src={AG} alt="AG logo"/>
+              <h1 className="Track-Name">agriculture</h1>
                 <div className='Track-Text'>
-                Stay tuned to learn more about our prize tracks for this year.
+                Participate in the VISTA Farms Food Future Innovate Track to become the next AgTech innovator. Create a project to make technology in agriculture more efficient, sustainable, and safer using the power of data-driven technologies.
                 </div>
               </Card>
               <Card style={{borderRadius: '30px'}} className='Track-Card'>
-              <img className='Track-Logo' src={Support} alt="Support logo"/>
-              <h1 className="Track-Name">Coming soon</h1>
+                <br/>
+              <img className='Track-Logo' src={Business} alt="Business logo"/>
+              <h1 className="Track-Name">business & finance</h1>
                 <div className='Track-Text'>
-                Stay tuned to learn more about our prize tracks for this year.
+                Support the growth of technology in the San Joaquin Valley by creating tech solutions for businesses - or starting your own! Increase the accessibility of financial services, financial or investment advisory, and financial literacy.
                 </div>
               </Card>
            </div>
