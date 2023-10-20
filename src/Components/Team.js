658dee918139fd8ae373c8a3b78f0162bfd9e66c
AlissonRoss <alisson@alisson.me>
2023-10-20 10:54:51 -0700

    Added new members to the Team component

--- src/Components/Team.js
+++ src/Components/Team.js
@@ -1,21 +1,32 @@
 import { Grid } from "@mui/material";
-import Jet from '../Assets/Team/jet.jpeg';
-import Porfi from '../Assets/Team/porfi.jpeg';
-import Alisson from '../Assets/Team/alisson.jpeg';
-import Mo from '../Assets/Team/mo.jpeg';
+import Jet from '../Assets/Team/jet.JPG';
+import Porfi from '../Assets/Team/porfi.JPG';
+import Alisson from '../Assets/Team/alisson.JPG';
+import Mo from '../Assets/Team/mo.JPG';
 import Ivan from '../Assets/Team/ivan.jpg'
 import Princess from '../Assets/Team/princess.jpeg';
 import Emi from '../Assets/Team/emi.jpg'
+import Noor from '../Assets/Team/noor.JPG'
+import Shyam from '../Assets/Team/shyam.JPG';
+import Akhil from '../Assets/Team/akhil.JPG';
+import Rafael from '../Assets/Team/rafael.JPG';
+import Laith from '../Assets/Team/laith.JPG';
+
 
 function Team() {
     const teamMembers = [
         { name: 'Porfirio Montoya', imgSrc: Porfi },
         { name: 'Alisson Ross', imgSrc: Alisson },
         { name: 'Jet Lin', imgSrc: Jet },
+        { name: 'Noor Irshad', imgSrc: Noor },
         { name: 'Mohamed Hasan', imgSrc: Mo },
+        { name: 'Shyam Gupta', imgSrc: Shyam },
+        { name: 'Akhil Devarasetty', imgSrc: Akhil },
+        { name: 'Rafael Lastre', imgSrc: Rafael },
+        { name: 'Laith Darras', imgSrc: Laith },
         { name: 'Ivan Piceno', imgSrc: Ivan },
         { name: 'Princess Thomas', imgSrc: Princess },
-        { name: 'Emi Rueth', imgSrc: Emi }
+        { name: 'Emi Rueth', imgSrc: Emi },
     ]
 
     return (
