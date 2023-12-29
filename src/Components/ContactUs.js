1ee70ee0ccd026f4054bbc77c3473090df53d9b1
mustachemo <mhasan1080@gmail.com>
2023-12-29 18:23:37 -0500

    Frontned portion done, but needs to be made resaponsive

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Container, TextField, Button, Box, Typography, CssBaseline, Paper } from '@mui/material';
+import { Container, TextField, Button, Box, Typography, Grid } from '@mui/material';
+import UCMmap from '../Assets/ucmmap.svg';
 
 const ContactPage = () => {
   const [formData, setFormData] = useState({
@@ -23,72 +24,84 @@ const ContactPage = () => {
 
   return (
     <>
-          <Box
-        sx={{
-          flexGrow: 1,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
+    <Grid container spacing={0} sx={{ height: '80vh', backgroundColor: 'background.default' }}>
+        <Grid item xs={12} sm={6} sx={{
           background: 'linear-gradient(to bottom, #342153 0%, #C0593A 50%, #D77B26 100%)',
-          p: 2
-        }}
-      >
-      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-        <Paper elevation={6} sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-          <Typography variant="h4" gutterBottom>
-            Contact Us
-          </Typography>
-          <Typography variant="body1">
-            Feel like contacting us? Submit your queries here and we will get back to you as soon as possible.
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              required
-              fullWidth
-              id="fullName"
-              label="Full Name"
-              name="fullName"
-              margin="normal"
-              value={formData.fullName}
-              onChange={handleChange}
-              autoComplete="name"
-              autoFocus
-            />
-            <TextField
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              margin="normal"
-              value={formData.email}
-              onChange={handleChange}
-              autoComplete="email"
-            />
-            <TextField
-              required
-              fullWidth
-              id="message"
-              label="Your Message"
-              name="message"
-              margin="normal"
-              value={formData.message}
-              onChange={handleChange}
-              multiline
-              rows={4}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2, bgcolor: 'secondary.main' }}
-            >
-              Submit
-            </Button>
-          </Box>
-        </Paper>
-      </Container>
-        </Box>
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <img src={UCMmap} alt="UCM Map" style={{ width: 400, height: 'auto' }} />
+        </Grid>
+        <Grid item xs={12} sm={6} sx={{
+          background: 'linear-gradient(to bottom, #342153 0%, #C0593A 50%, #D77B26 100%)',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}>
+            <Container component="main" maxWidth="sm" sx={{ mb: 4,
+                color: 'white'
+            }}>
+                <Typography variant="h4" gutterBottom>
+                    Contact Us
+                </Typography>
+                <Typography variant="body1">
+                    Feel like contacting us? Submit your queries here and we will get back to you as soon as possible.
+                </Typography>
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1, 
+                    bgcolor: 'background.paper',
+                    borderRadius: 1,
+                    // make left and right paddding the same as the top and bottom padding
+                    padding: (theme) => `${theme.spacing(2)} ${theme.spacing(3)}`,
+                }}>
+                    <TextField
+                    required
+                    fullWidth
+                    id="fullName"
+                    label="Full Name"
+                    name="fullName"
+                    margin="normal"
+                    value={formData.fullName}
+                    onChange={handleChange}
+                    autoComplete="name"
+                    autoFocus
+                    />
+                    <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    margin="normal"
+                    value={formData.email}
+                    onChange={handleChange}
+                    autoComplete="email"
+                    />
+                    <TextField
+                    required
+                    fullWidth
+                    id="message"
+                    label="Your Message"
+                    name="message"
+                    margin="normal"
+                    value={formData.message}
+                    onChange={handleChange}
+                    multiline
+                    rows={4}
+                    />
+                    <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2, bgcolor: 'secondary.main' }}
+                    >
+                    Submit
+                    </Button>
+                </Box>
+            </Container>
+        </Grid>
+    </Grid>
+    {/* </Box> */}
     </>
   );
 };
