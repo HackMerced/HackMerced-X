ad465a0437257834a111ed03b41af95374b77885
mustachemo <mhasan1080@gmail.com>
2023-12-29 16:00:11 -0500

    Frontend partially done

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,11 +1,63 @@
-function ContactUs() {
-    return (
-      <div>
-        <h1>Contact Us</h1>
-        <p>For any questions or concerns, please email us at</p>
+import React, { useState } from 'react';
+import './ContactUs.css';
 
-      </div>
-    );
-  }
-  
-  export default ContactUs;
+const ContactPage = () => {
+  const [formData, setFormData] = useState({
+    fullName: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // functionality to be put in soon
+    console.log(formData);
+  };
+
+  return (
+    <div className="contact-container">
+      <main className="main-content">
+        <section className="contact-section">
+          <h1>Contact Us</h1>
+          <p>Feel like contacting us? Submit your queries here and we will get back to you as soon as possible.</p>
+          <form onSubmit={handleSubmit} className="contact-form">
+            <input
+              type="text"
+              name="fullName"
+              placeholder="Full Name"
+              value={formData.fullName}
+              onChange={handleChange}
+              className="form-input"
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+              className="form-input"
+            />
+            <textarea
+              name="message"
+              placeholder="Your message here..."
+              value={formData.message}
+              onChange={handleChange}
+              className="form-textarea"
+            ></textarea>
+            <button type="submit" className="submit-button">Submit</button>
+          </form>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default ContactPage;
