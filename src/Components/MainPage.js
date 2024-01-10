774e54c261ce1723c6891262e6fc99f3322083ad
Jet Lin <jetlin101@gmail.com>
2024-01-10 22:23:39 +0800

    Fixed stars, added glide in animation

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -11,12 +11,20 @@ function MainPage() {
                   March 8-10 2024 @ UC Merced
                 </p>
                
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLScZoaMgds1Ihb4j7VfD3NQQSOSvsBOuurg8Lz-hIB7Im71Lug/viewform">
+                <a className="registerButton2Stars" href="https://docs.google.com/forms/d/e/1FAIpQLScZoaMgds1Ihb4j7VfD3NQQSOSvsBOuurg8Lz-hIB7Im71Lug/viewform">
                   <button className="registerButton2">Register Now</button>
                   
-                  {/*
-                  <p className="star1" id="star">★</p><p className="star2" id="star">★</p><p className="star3" id="star">★</p>
-                  */}
+                  <div className="starWrapper">
+                    <p className="star1" id="star">★</p>
+                    <p className="star2" id="star">★</p>
+                    <p className="star3" id="star">★</p>
+                  </div>
+                  <div className="starBursts">
+                    <p className="starBurst1">✨</p>
+                    <p className="starBurst2">⭐️</p>
+                    <p className="starBurst3">🌟</p>
+                  </div>
+                 
                   
                 </a>
                 <p className="Main-footer">Have questions? Email <a className="Main-footerlink" href="mailto:general@hackmerced.com"> general@hackmerced.com <p className="emailEmoji">📧</p></a></p>
