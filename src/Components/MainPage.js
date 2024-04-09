72515579799c37896ca702f2fa84ad133b9dbe7e
Jet Lin <jetlin101@gmail.com>
2024-04-09 12:53:03 -0700

    added applicatoin due date

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -6,14 +6,14 @@ function MainPage() {
               <div className="Main-text">
                 <h1 className="Main-title">HackMerced X</h1>
                 <p className="Main-subtitle">
-                  Thank you for attending HackMerced IX the largest hackathon in the San Joaquin Valley
+                  largest hackathon in the San Joaquin Valley
                   <br/>
                   HackMerced X Coming Soon 2025 @ UC Merced
                 </p>
                 <p className="Main-subtitle">
                   Applications to join our team are Open!
                   <br/>
-                  Apply Now to help make HackMerced X come to fruition!
+                  Apply before April 20th to help make HackMerced X come to fruition!
                 </p>
                 <a className="registerButton2Stars" href="https://docs.google.com/forms/d/e/1FAIpQLSd7K6NUy1IWhxdTOlP_40LQLvO-9S1cIRlE_KQlPj6xkFf3dg/viewform?usp=sf_link">
                   <button className="registerButton2">Apply Now!</button>
