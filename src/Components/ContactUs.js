dec5dd142b420d0307ba00171c62ad3336fd71b0
Andre Lew <118043566+aelew@users.noreply.github.com>
2024-09-22 17:00:49 -0700

    add new contact form

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,160 +1,87 @@
-import React, { useState } from 'react';
-import { Container, TextField, Button, Box, Typography, Grid } from '@mui/material';
-import UCMmap from '../Assets/ucmmap.svg';
-import { collection, addDoc, setDoc, doc } from "firebase/firestore"; 
-import db from '../firebase.js';
+import React from 'react';
+import { Container, Box, Typography, Grid } from '@mui/material';
+import ucmMap from '../Assets/ucmmap.svg';
 
 const ContactPage = () => {
-  const emailRegex = /\S+@\S+\.\S+/;
-  const [errors, setErrors] = useState({});
-
-  const [formData, setFormData] = useState({
-    fullName: '',
-    email: '',
-    message: '',
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-
-    setErrors(prevErrors => ({ ...prevErrors, [name]: '' }));
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const validateEmail = (email) => {
-    if (!email) {
-      return "Email is required.";
-    }
-    if (!emailRegex.test(email)) {
-      return "Please enter a valid email.";
-    }
-    return '';
-  };
-
-  const handleSubmit = async (e) => {
-    console.log(`formData: ${formData.fullName}`)
-
-
-    try {
-      e.preventDefault();
-      const emailError = validateEmail(formData.email);
-      if (emailError) {
-        setErrors({ ...errors, email: emailError });
-        return;
-      }
-
-      const uniqueDocId = `${formData.fullName}_${Date.now()}`;
-
-      const colRef = doc(db, "contacts", uniqueDocId);
-
-
-      await setDoc(colRef, {
-        fullName: formData.fullName,
-        email: formData.email,
-        message: formData.message,
-        submittedAt: new Date()
-      });
-
-      console.log("Document written successfully!");
-
-      setFormData({
-        fullName: '',
-        email: '',
-        message: '',
-      });
-
-    } catch (error) {
-      console.error("Error adding document: ", error);
-
-    }
-  };
-
   return (
-    <>
-    <Grid container spacing={0} className="Contact-Us-container" sx={{ backgroundColor: 'background.default' }}>
-      <Grid item xs={12} sm={6} sx={{
-        background: 'linear-gradient(to bottom, #342153 0%, #C0593A 50%, #D77B26 100%)',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
-      <img src={UCMmap} alt="UCM Map" style={{ width: 400, height: 'auto' }} />
-      </Grid>
-      <Grid item xs={12} sm={6} sx={{
-        background: 'linear-gradient(to bottom, #342153 0%, #C0593A 50%, #D77B26 100%)',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}>
-          <Container component="main" maxWidth="sm" sx={{ mb: 4,
-              color: 'white'
-          }}>
-              <Typography variant="h4" gutterBottom>
-                Contact Us
-              </Typography>
-              <Typography variant="body1">
-                We would love to hear from you! Please fill out the form below and we will get back to you as soon as possible.
-              </Typography>
-              <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1, 
+    <div
+      style={{
+        background:
+          'linear-gradient(to bottom, #342153 0%, #C0593A 50%, #D77B26 100%)'
+      }}
+    >
+      <Grid
+        container
+        spacing={0}
+        className="Contact-Us-container"
+        sx={{
+          marginX: 'auto',
+          maxWidth: '1280px'
+        }}
+      >
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          mt={2}
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+        >
+          <img
+            src={ucmMap}
+            alt="UCM Map"
+            style={{ height: 400, width: 'auto' }}
+          />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          my={6}
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+        >
+          <Container component="main" maxWidth="sm" sx={{ color: 'white' }}>
+            <Typography variant="h4" gutterBottom>
+              Contact Us
+            </Typography>
+            <Typography variant="body1">
+              We would love to hear from you! Please fill out the form below and
+              we will get back to you as soon as possible.
+            </Typography>
+            <Box
+              sx={{
+                mt: 3,
                 bgcolor: 'background.paper',
                 borderRadius: 1,
-                padding: (theme) => `${theme.spacing(2)} ${theme.spacing(3)}`,
-              }}>
-                <TextField
-                  required
-                  fullWidth
-                  id="fullName"
-                  label="Full Name"
-                  name="fullName"
-                  margin="normal"
-                  value={formData.fullName}
-                  onChange={handleChange}
-                  autoComplete="name"
-                  autoFocus
-                />
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  margin="normal"
-                  value={formData.email}
-                  onChange={handleChange}
-                  error={!!errors.email}
-                  helperText={errors.email || ''}
-                  autoComplete="email"
-                  type="email"
-                />
-                <TextField
-                  required
-                  fullWidth
-                  id="message"
-                  label="Your Message"
-                  name="message"
-                  margin="normal"
-                  value={formData.message}
-                  onChange={handleChange}
-                  multiline
-                  rows={4}
-                />
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2, bgcolor: 'secondary.main' }}
-                  >
-                  Submit
-                </Button>
-              </Box>
+                padding: (theme) =>
+                  `${theme.spacing(2)} ${theme.spacing(2)} 0 ${theme.spacing(
+                    2
+                  )}`
+              }}
+            >
+              <iframe
+                src="https://tally.so/embed/nPYQEB?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+                title="Contact Us"
+                loading="lazy"
+                width="100%"
+                frameBorder={0}
+                marginHeight={0}
+                marginWidth={0}
+                height={495}
+              />
+            </Box>
           </Container>
+        </Grid>
       </Grid>
-    </Grid>
-    {/* </Box> */}
-    </>
+      {/* </Box> */}
+    </div>
   );
 };
 
