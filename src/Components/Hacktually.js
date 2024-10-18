f40e2d8f69fd29f70e28b2ccd1a88c0f4bb996d2
Kai Wildberger <kiawildberger@gmail.com>
2024-10-18 09:59:13 -0700

    update hours and community discord link

--- src/Components/Hacktually.js
+++ src/Components/Hacktually.js
@@ -4,10 +4,10 @@ function Hacktually() {
         <h2 className ="Hacktually-title">hacktually</h2>
         <div className="Hacktually-Grid">
           <div className="Hacktually-info">Hacktually is a workshop-based day of learning to prepare for the upcoming HackMerced X and to meet like-minded coders and engineers.</div>
-          <div className="Hacktually-info">UC Merced COB2 110, 130 and 140 <br/>Saturday, November 2nd 9am-5pm</div>
-          <div className="Hacktually-info">Food will be available at noon</div>
+          <div className="Hacktually-info">UC Merced COB2 110, 130 and 140 <br/>Saturday, November 2nd 10am-6pm<br/>Food will be available at noon</div>
+          <div className="Hacktually-info">Join our <a href="https://discord.gg/ShRCuvHcYk" target="_blank" rel="noreferrer">Discord</a> to stay up to date</div>
           <div className="Hacktually-info">Learn CSS, UI/UX, NodeJS, React, Git, Bash, Firebase and more!</div>
-        </div> 
+        </div>
       </div>
     );
   }
