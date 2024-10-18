cbf3be30341a24c87b733f66bbdf6c8dc5d5098b
Kai Wildberger <kiawildberger@gmail.com>
2024-10-18 09:46:51 -0700

    update main page wording for hacktually and add hacktually section

---
+++ src/Components/Hacktually.js
@@ -0,0 +1,15 @@
+function Hacktually() {
+    return (
+      <div className="Hacktually" id="hacktually">
+        <h2 className ="Hacktually-title">hacktually</h2>
+        <div className="Hacktually-Grid">
+          <div className="Hacktually-info">Hacktually is a workshop-based day of learning to prepare for the upcoming HackMerced X and to meet like-minded coders and engineers.</div>
+          <div className="Hacktually-info">UC Merced COB2 110, 130 and 140 <br/>Saturday, November 2nd 9am-5pm</div>
+          <div className="Hacktually-info">Food will be available at noon</div>
+          <div className="Hacktually-info">Learn CSS, UI/UX, NodeJS, React, Git, Bash, Firebase and more!</div>
+        </div> 
+      </div>
+    );
+  }
+  
+  export default Hacktually;
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -3,12 +3,14 @@ import MainPage from './MainPage.js';
 import Tracks from './Tracks.js';
 import FAQ from './FAQ.js';
 import Sponsors from './Sponsors.js';
+import Hacktually from './Hacktually.js';
 
 function HomePage() {
   return (
     <div>
       <MainPage/>
       <Tracks/>
+      <Hacktually/>
       <FAQ/>
       <Sponsors/>
     </div>
