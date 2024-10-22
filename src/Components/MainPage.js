b7b1d6291ea542928cf21aba1ad49bb272eb87ed
Kai Wildberger <kiawildberger@gmail.com>
2024-10-22 14:32:06 -0700

    fix text scaling in navbar button

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -13,9 +13,18 @@ function MainPage() {
                   <br/>
                   Learn from our workshops and connect with like-minded students and coders
                 </p>
-                <a className="registerButton2" href="./#hacktually">
-                {/* <a className="registerButton2" onClick={scrollToHacktually()}> */}
+                <a className="registerButton2Stars" href="./#hacktually">
                   <button className="registerButton2">Learn more</button>
+                  {/* <div class="starWrapper">
+                    <p class="star1" id="star">★</p>
+                    <p class="star2" id="star">★</p>
+                    <p class="star3" id="star">★</p>
+                  </div>
+                  <div class="starBursts">
+                    <p class="starBurst1">✨</p>
+                    <p class="starBurst2">⭐️</p>
+                    <p class="starBurst3">🌟</p>
+                  </div> */}
                 </a>
                 <p className="Main-footer">Have questions? Email <a className="Main-footerlink" href="mailto:general@hackmerced.com"> general@hackmerced.com <p className="emailEmoji">📧</p></a></p>
                 </div>
