77f98b46b8c37e80a60484a7ef6e81fc839dd801
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-10-23 20:11:59 -0700

    make rough draft for first page

--- src/Components/Sponsorship-Deck/FirstPage.js
+++ src/Components/Sponsorship-Deck/FirstPage.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import HackMercedTower from './assets/hackmerced-tower.png';
+import BackgroundBuildings from './assets/background-buildings.png';
 
 const FirstPage = () => {
     return (
         <section className='contain-first-page'>
+            <div className="first-page--left-side">
+                <div className="contain-gradients">
+                    <div className='gradient1' />
+                    <div className='gradient2' />
+                </div>
+                <img src={HackMercedTower} className='hackmerced-tower' alt="hack merced tower" />
+                <img src={BackgroundBuildings} className="background-buildings" alt="background buildings" />
+            </div>
+            <div className="first-page--right-side">
 
+            </div>
         </section>
     )
 };
