2e5847d12001acbfcbd091407a937c5ea8ec38d5
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-10-27 14:36:41 -0700

    install react-icons and improve layout of first page

--- src/Components/Sponsorship-Deck/FirstPage.js
+++ src/Components/Sponsorship-Deck/FirstPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import HackMercedTower from './assets/hackmerced-tower.png';
 import BackgroundBuildings from './assets/background-buildings.png';
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaSquareInstagram } from "react-icons/fa6";
 
 const FirstPage = () => {
     return (
         <section className='contain-first-page'>
-            <div className="first-page--left-side">
+            <div className="first-page--box">
                 <div className="contain-gradients">
                     <div className='gradient1' />
                     <div className='gradient2' />
@@ -13,8 +16,27 @@ const FirstPage = () => {
                 <img src={HackMercedTower} className='hackmerced-tower' alt="hack merced tower" />
                 <img src={BackgroundBuildings} className="background-buildings" alt="background buildings" />
             </div>
-            <div className="first-page--right-side">
-
+            <div className="first-page--box">
+                <div className="contain-gradients">
+                    <div className='gradient1' />
+                    <div className='gradient2' />
+                </div>
+                <img src={BackgroundBuildings} className="background-buildings2" alt="background buildings" />
+                <div className='text-contents'>
+                    <div className='social-link'>
+                        <FaFacebookSquare className='socials-logo' />
+                        <h2>HackMerced</h2>
+                    </div>
+                    <div className='social-link'>
+                        <FaSquareXTwitter className='socials-logo' />
+                        <h2>@HackMerced</h2>
+                    </div>
+                    <div className='social-link'>
+                        <FaSquareInstagram className='socials-logo' />
+                        <h2>@HackMerced</h2>
+                    </div>
+                    
+                </div>
             </div>
         </section>
     )
