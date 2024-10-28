2ecb11fb41263f5221cae949a120cb4c74052e5d
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-10-27 17:24:33 -0700

    add links to socials and sliding animation

--- src/Components/Sponsorship-Deck/FirstPage.js
+++ src/Components/Sponsorship-Deck/FirstPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HackMercedTower from './assets/hackmerced-tower.png';
 import BackgroundBuildings from './assets/background-buildings.png';
 import { FaFacebookSquare } from "react-icons/fa";
@@ -8,7 +9,7 @@ import { FaSquareInstagram } from "react-icons/fa6";
 const FirstPage = () => {
     return (
         <section className='contain-first-page'>
-            <div className="first-page--box">
+            <div className="first-page-box">
                 <div className="contain-gradients">
                     <div className='gradient1' />
                     <div className='gradient2' />
@@ -16,26 +17,32 @@ const FirstPage = () => {
                 <img src={HackMercedTower} className='hackmerced-tower' alt="hack merced tower" />
                 <img src={BackgroundBuildings} className="background-buildings" alt="background buildings" />
             </div>
-            <div className="first-page--box">
+
+            <div className="first-page-box">
                 <div className="contain-gradients">
                     <div className='gradient1' />
-                    <div className='gradient2' />
+                    <div className='gradient3' />
                 </div>
                 <img src={BackgroundBuildings} className="background-buildings2" alt="background buildings" />
                 <div className='text-contents'>
-                    <div className='social-link'>
-                        <FaFacebookSquare className='socials-logo' />
-                        <h2>HackMerced</h2>
-                    </div>
-                    <div className='social-link'>
-                        <FaSquareXTwitter className='socials-logo' />
-                        <h2>@HackMerced</h2>
-                    </div>
-                    <div className='social-link'>
-                        <FaSquareInstagram className='socials-logo' />
-                        <h2>@HackMerced</h2>
-                    </div>
-                    
+                    <a href='https://www.facebook.com/hackmerced' className='first-page-links'>
+                        <div className='social-link first-page-link-1'>
+                            <FaFacebookSquare className='socials-logo' />
+                            <h2>HackMerced</h2>
+                        </div>
+                    </a>
+                    <a href='https://x.com/hackmerced' className='first-page-links'>
+                        <div className='social-link first-page-link-2'>
+                            <FaSquareXTwitter className='socials-logo' />
+                            <h2>@HackMerced</h2>
+                        </div>
+                    </a>
+                    <a href='https://www.instagram.com/hackmerced/' className='first-page-links'>
+                        <div className='social-link first-page-link-3'>
+                            <FaSquareInstagram className='socials-logo' />
+                            <h2>@HackMerced</h2>
+                        </div>
+                    </a>
                 </div>
             </div>
         </section>
