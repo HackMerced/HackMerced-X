23c640bc2c3f85fea91c661dddb02d17a00eb376
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-10-27 22:14:04 -0700

    begin draft for second page of sponsor us page

--- src/Components/Sponsorship-Deck/SecondPage.js
+++ src/Components/Sponsorship-Deck/SecondPage.js
@@ -3,7 +3,22 @@ import React from 'react';
 const SecondPage = () => {
     return (
         <section className='contain-second-page'>
+            <h1>Why Sponsor HackMerced?</h1>
+            <div className='second-page-text-line'>
+                <div className='second-page-text-box1'>
 
+                </div>
+            </div>
+            <div className='second-page-text-line'>
+                <div className='second-page-text-box2'>
+
+                </div>
+            </div>
+            <div className='second-page-text-line'>
+                <div className='second-page-text-box3'>
+
+                </div>
+            </div>
         </section>
     )
 };
