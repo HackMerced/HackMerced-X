3b45891c92e52782c0714fc5bce93cfd66485ea1
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-11-01 13:24:56 -0700

    design bronze tier

--- src/Components/Sponsorship-Deck/ThirdPage.js
+++ src/Components/Sponsorship-Deck/ThirdPage.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import bronze from './assets/bronze-tier.png';
 
 const ThirdPage = () => {
     return (
         <section className='contain-third-page'>
             <h1>Sponsorship Options</h1>
             <div className='sponsor-tiers'>
-                <h3>Bronze Tier</h3>
-                <div className='tier-items'>
-                    <p>Send Mentors</p>
-                    <p>Thanked in Ceremonies</p>
-                    <p>Sponsor a Prize</p>
-                    <p>Distribute Merch</p>
-                    <p>Logo on Website</p>
+                <img src={bronze} />
+                <div className='tier-description'>
+                    <h3>Bronze Tier ($500+)</h3>
+                    <div className='tier-items'>
+                        <div className='tier-items-row'>
+                            <div className='text-background'><p>Send Mentors</p></div>
+                            <div className='text-background'><p>Thanked in Ceremonies</p></div>
+                        </div>
+                        <div className='tier-items-row'>
+                            <div className='text-background'><p>Sponsor a Prize</p></div>
+                            <div className='text-background'><p>Distribute Merch</p></div>
+                            <div className='text-background'><p>Logo on Website</p></div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className='sponsor-tiers'>
