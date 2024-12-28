f1578e7a55b540d7a9a2d9faecc258670a005318
Jet Lin <jetlin101@gmail.com>
2024-12-28 10:59:45 -0800

    removed hacktually, discord button

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -2,14 +2,14 @@ import React from 'react';
 import MainPage from './MainPage.js';
 import Tracks from './Tracks.js';
 import FAQ from './FAQ.js';
-import Hacktually from './Hacktually.js';
+// import Hacktually from './Hacktually.js';
 // import Sponsors from './Sponsors.js';
 
 function HomePage() {
   return (
     <div>
       <MainPage/>
-      <Hacktually/>
+      {/* <Hacktually/> */}
       <Tracks/>
       <FAQ/>
       {/* <Sponsors/> */}
