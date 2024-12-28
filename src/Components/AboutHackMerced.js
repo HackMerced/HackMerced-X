45e60f6b45964f9af90e0206be436ee93c1191ed
Jet Lin <jetlin101@gmail.com>
2024-12-28 15:32:17 -0800

    white background fix 3 jet

--- src/Components/AboutHackMerced.js
+++ src/Components/AboutHackMerced.js
@@ -5,13 +5,19 @@ import React from 'react';
 import TeamPhoto from '..//Assets/Team/HM10Team/Team2.webp';
 
 import { Card } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const StyledCard = styled(Card)(({ theme }) => ({
+    background: 'linear-gradient(170.14deg, var(--main-shadows-2) 1.13%, var(--secondary-shadows) 104.83%)',
+}));
+
 export default function AboutHackMerced(){
     return(
         <div className="About-HM">
             <div className="About-HM-Container">
                 <h2 className='About-HM-Title'>about hackmerced</h2>
                 <div className="About-HM-Contents">
-                    <Card className="About-HM-Desc">
+                    {/* <Card className="About-HM-Desc">
                         <div className="About-HM-Text">HackMerced is a 36-hour hackathon hosted at the
                         University of California, Merced by an organization
                         comprised of UC Merced students and alumni. Students
@@ -21,7 +27,19 @@ export default function AboutHackMerced(){
                         environment that showcases the creativity and innovation
                         culminating in the San Joaquin Valley.</div>
                         
-                    </Card>
+                    </Card> */}
+                    <StyledCard className="About-HM-Desc">
+                        <div className="About-HM-Text">
+                            HackMerced is a 36-hour hackathon hosted at the
+                            University of California, Merced by an organization
+                            comprised of UC Merced students and alumni. Students
+                            from all schools and backgrounds, regardless of prior
+                            experience in computer science, are invited to participate.
+                            We aim to create a collaborative and interdisciplinary
+                            environment that showcases the creativity and innovation
+                            culminating in the San Joaquin Valley.
+                        </div>
+                    </StyledCard>
                     <img className='Team-photo' src={TeamPhoto} alt="HackMerced Team"/>
                 </div>
             </div>
