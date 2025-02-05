b8b32a5f9b504bb4e8db7d5f970438c240dabaa8
Brandon D. <alyssajoseph415@gmail.com>
2025-02-04 23:44:21 -0800

    Fixed the First Page email issue and updated package-lock.json

--- src/Components/Sponsorship-Deck/FirstPage.js
+++ src/Components/Sponsorship-Deck/FirstPage.js
@@ -42,6 +42,12 @@ const FirstPage = () => {
                 </div>
                 <img src={BackgroundBuildings} className="background-buildings2" alt="background buildings" />
                 <div className='text-contents'>
+                    <a href='mailto:sponsors@hackmerced.com' className='first-page-links'>
+                        <div className='social-link first-page-link-0'>
+                            <FaEnvelope className='socials-logo' />
+                            <h2>Email Us</h2>
+                        </div>
+                    </a>
                     <a href='https://hackmerced.com/discord' className='first-page-links'>
                         <div className='social-link first-page-link-1'>
                             <FaDiscord className='socials-logo' />
